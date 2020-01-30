34da7e00fbaf866c4887e554a083be89754aa831
anasbenyaiche <anas.benyaiche@yahoo.fr>
2020-01-30 21:32:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,79 +1,30 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
 
-export default class App extends React.Component {
 
-    // we use a constructor to initialize our state
-  constructor(props) {
-    super(props);
+export default class App extends React.Component{
+    constructor(props){
+        super(props);{
+            this.state={
+                task:"",
+                todolist:["cleaning the cars", "watching movie", "reading book"]
+            }
 
-    // we initiatied the state here with no value 
-    this.state = {
-      name: "",
-      age: "",
-      email: "",
-      location: ""
-    };
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="form">
-          <div className="input-container">
-            <span>name:</span>
-
-            <input
-              onChange={event =>
-                this.setState({
-                  name: event.target.value
-                })
-              }
-              value={this.state.name}
-              placeholder="tape your name"
-            />
-          </div>
-          <div className="input-container">
-            <span>email:</span>
-            <input
-              value={this.state.email}
-              onChange={event => {
+        }
+    }
+    render(){
+        return (<div>
+            <div style={{border:"3px solid black "}}>
+                <input />
+                <button>+</button>
+            </div>
+            <div>
+                <ul>
+        {this.state.todolist.map(task => <li>{task}</li> )}
+                </ul>
                 
-                this.setState({ email: event.target.value })}}
-              placeholder="tape your email"
-            />
-          </div>
-          <div className="input-container">
-            <span>age:</span>
-            <input
-              value={this.state.age}
-              onChange={event => this.setState({ age: event.target.value })}
-              placeholder="tape your age"
-            />
-          </div>
-          <div className="input-container">
-            <span>location:</span>
-            <input
+            </div>
 
-                // the event to change the value of the input
-              onChange={event =>
-                this.setState({ location: event.target.value })
-              }
 
-            //   the value of the input is reliated to state automatically change when we change the value
-              value={this.state.location}
-              placeholder="tape your location"
-            />
-          </div>
-        </div>
-        <div className="profil">
-            {/* we use the state which is a an object contains a name, age, email and location to output the result */}
-          <h1>{this.state.name}</h1>
-          <h1>{this.state.email}</h1>
-          <h1>{this.state.location}</h1>
-          <h1>{this.state.age}</h1>
-        </div>
-      </div>
-    );
-  }
+        </div>)
+    }
 }
